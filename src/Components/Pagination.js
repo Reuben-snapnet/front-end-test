dd674a938068db8c0df55fc51b3b494a4147d75f
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-08-28 13:50:43 +0100

    first

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -16,19 +16,19 @@ const Pagination = () => {
       <ul className="flex">
         <li
           onClick={() => setCurrentPage(1)}
-          className="py-2 px-3 cursor-pointer border-2 border-gray-500"
+          className="py-2 px-3 cursor-pointer border-2 border-gray-500 hover:bg-blue-300"
         >
           1
         </li>
         <li
           onClick={() => setCurrentPage(2)}
-          className="py-2 px-3 cursor-pointer border-2 border-gray-500 mx-2"
+          className="py-2 px-3 cursor-pointer border-2 border-gray-500 hover:bg-blue-300 mx-2"
         >
           2
         </li>
         <li
           onClick={() => setCurrentPage(3)}
-          className="py-2 px-3 cursor-pointer border-2 border-gray-500"
+          className="py-2 px-3 cursor-pointer border-2 border-gray-500 hover:bg-blue-300"
         >
           3
         </li>
