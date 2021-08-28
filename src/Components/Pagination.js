1e711f71699f719eb3bef6a1568c56912322f76e
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-08-28 13:38:51 +0100

    first

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,28 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { EventContext } from "./EventProvider";
 
 const Pagination = () => {
-    const [currentEvents, , postsPerPage, , paginate] = useContext(EventContext)
+  const [currentEvents, , postsPerPage, paginate, setCurrentPage] = useContext(
+    EventContext
+  );
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(currentEvents.length / postsPerPage); i++) {
     pageNumbers.push(i);
   }
 
-  console.log();
+  console.log(paginate);
 
   return (
-    <nav>
-      <ul className=''>
-        {pageNumbers.map(number => (
-          <li key={number} className='border'>
-            <a onClick={() => paginate(number)} href='!#' className=''>
-              {number}
-            </a>
-          </li>
-        ))}
+    <div className="flex justify-center">
+      <ul className="flex">
+        <li
+          onClick={() => setCurrentPage(1)}
+          className="py-2 px-3 cursor-pointer border-2 border-gray-500"
+        >
+          1
+        </li>
+        <li
+          onClick={() => setCurrentPage(2)}
+          className="py-2 px-3 cursor-pointer border-2 border-gray-500 mx-2"
+        >
+          2
+        </li>
+        <li
+          onClick={() => setCurrentPage(3)}
+          className="py-2 px-3 cursor-pointer border-2 border-gray-500"
+        >
+          3
+        </li>
       </ul>
-    </nav>
+    </div>
   );
 };
 
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            About
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3 className="pt-10 font-semibold text-2xl text-center"> About us</h3>
+    </div>
+  );
+};
 
-export default About
+export default About;
